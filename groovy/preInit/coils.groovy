eb3e5b4a9ed6f4147f0fd80d9acc5c1c1ed0d7ea
LunaFreyjaNox <romain.caudy@gmail.com>
2024-03-16 13:23:40 +0100

    Add new coils  with textures

---
+++ groovy/preInit/coils.groovy
@@ -0,0 +1,6 @@
+content.createBlock("darkmatter_coil").register();
+content.createBlock("darkenergyalloy_coil").register();
+content.createBlock("runicalloy_coil").register();
+content.createBlock("stasis_coil").register();
+content.createBlock("stellarite_coil").register();
+content.createBlock("wakfustasis_coil").register();
